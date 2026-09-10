d1792ff7c569f40c83787c7f538126b2b4481cc7
lucaslive974 <lucasribeirolima974@gmail.com>
2026-09-10 17:24:41 -0300

    [chore]: Add test coverage targets to CMake.

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,14 +1,77 @@
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake")
 
+include(CTest)
 include("gtest")
 
-file(GLOB_RECURSE CTCI_TEST_SOURCE "tests/*.cpp")
-enable_testing()
+file(GLOB_RECURSE CTCI_TEST_SOURCE CONFIGURE_DEPENDS "tests/*.cpp")
 
 add_executable(CTCI_TEST ${CTCI_TEST_SOURCE})
 target_include_directories(CTCI_TEST PRIVATE "include")
-target_link_libraries(CTCI_TEST CTCI GTest::gtest_main)
+option(ENABLE_COVERAGE "Enable code coverage instrumentation" OFF)
+
+if(ENABLE_COVERAGE)
+    target_link_libraries(CTCI_TEST PRIVATE "$<LINK_LIBRARY:WHOLE_ARCHIVE,CTCI>" GTest::gtest_main)
+    enable_coverage_instrumentation(CTCI)
+else()
+    target_link_libraries(CTCI_TEST PRIVATE CTCI GTest::gtest_main)
+endif()
 
 include(GoogleTest)
-gtest_discover_tests(CTCI_TEST)
+gtest_discover_tests(CTCI_TEST
+    PROPERTIES ENVIRONMENT "LLVM_PROFILE_FILE=${CMAKE_BINARY_DIR}/coverage_%p.profraw"
+)
 
+# --- Coverage Target Generation ---
+set(COVERAGE_REPORT_DIR "${CMAKE_BINARY_DIR}/coverage")
+
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    # Resolve version-matched or generic LLVM tools
+    find_program(LLVM_PROFDATA_TOOL NAMES llvm-profdata llvm-profdata-${CMAKE_CXX_COMPILER_VERSION})
+    find_program(LLVM_COV_TOOL NAMES llvm-cov llvm-cov-${CMAKE_CXX_COMPILER_VERSION})
+
+    if(LLVM_PROFDATA_TOOL AND LLVM_COV_TOOL)
+        add_custom_target(generate-coverage-report
+            COMMAND ${CMAKE_COMMAND} -E rm -f ${CMAKE_BINARY_DIR}/*.profraw ${CMAKE_BINARY_DIR}/coverage.profdata
+            COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
+            COMMAND ${LLVM_PROFDATA_TOOL} merge -sparse
+                    ${CMAKE_BINARY_DIR}/*.profraw
+                    -o ${CMAKE_BINARY_DIR}/coverage.profdata
+            COMMAND ${LLVM_COV_TOOL} export $<TARGET_FILE:CTCI_TEST>
+                    -instr-profile=${CMAKE_BINARY_DIR}/coverage.profdata
+                    -format=lcov
+                    --ignore-filename-regex="tests/|.*_deps.*"
+                    > ${CMAKE_BINARY_DIR}/lcov.info
+            COMMAND ${LLVM_COV_TOOL} show $<TARGET_FILE:CTCI_TEST>
+                    -instr-profile=${CMAKE_BINARY_DIR}/coverage.profdata
+                    -format=html
+                    -output-dir=${COVERAGE_REPORT_DIR}
+                    --ignore-filename-regex="tests/|.*_deps.*"
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+            DEPENDS CTCI_TEST
+            COMMENT "Generating LLVM coverage report..."
+        )
+    endif()
+
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    find_program(LCOV_TOOL lcov)
+    find_program(GENHTML_TOOL genhtml)
+
+    if(LCOV_TOOL AND GENHTML_TOOL)
+        add_custom_target(generate-coverage-report
+            COMMAND ${LCOV_TOOL} --directory ${CMAKE_BINARY_DIR} --zerocounters
+            COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
+
+            # Capture gcov tracefile from build directory
+            COMMAND ${LCOV_TOOL} --directory ${CMAKE_BINARY_DIR} --capture --output-file ${CMAKE_BINARY_DIR}/coverage.info
+
+            # Filter out external, system headers, tests, and third-party dependencies
+            COMMAND ${LCOV_TOOL} --remove ${CMAKE_BINARY_DIR}/coverage.info '/usr/*' '*/tests/*' '*/_deps/*' --output-file ${CMAKE_BINARY_DIR}/lcov.info
+
+            # Generate HTML dashboard
+            COMMAND ${GENHTML_TOOL} ${CMAKE_BINARY_DIR}/lcov.info --output-directory ${COVERAGE_REPORT_DIR}
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+            DEPENDS CTCI_TEST
+            COMMENT "Generating GCC/gcov coverage report with lcov..."
+        )
+    endif()
+endif()
